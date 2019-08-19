76cd5f81e8a3be1c3d46954a57ea32c6518dfb7f
Rik Lamers <info@riklamers.com>
2019-08-19 03:06:23 +0200

    Added support for img + webp converter

--- webpack.config.js
+++ webpack.config.js
@@ -4,20 +4,21 @@ const MiniCssExtractPlugin = require('mini-css-extract-plugin');
 const HtmlWebpackPlugin = require('html-webpack-plugin');
 const WebpackMd5Hash = require('webpack-md5-hash');
 const { CleanWebpackPlugin } = require('clean-webpack-plugin');
+const ImageminWebpWebpackPlugin= require("imagemin-webp-webpack-plugin");
 
 module.exports = {
     entry: {
         main: './src/index.js'
     },
-        output: {
-            path: path.resolve(__dirname, 'build'),
-            filename: 'js/[name].[chunkhash].js'
-        },
-        target: 'node',
-        externals: [
-            nodeExternals()
-        ],
-        module: {
+    output: {
+        path: path.resolve(__dirname, 'build'),
+        filename: 'js/[name].[chunkhash].js'
+    },
+    target: 'node',
+    externals: [
+        nodeExternals()
+    ],
+    module: {
         rules: [
             {
                 test: /\.js$/,
@@ -35,6 +36,45 @@ module.exports = {
                     'postcss-loader',
                     'sass-loader'
                 ]
+            },
+            {
+                test: /\.(gif|png|jpe?g|svg)$/i,
+                use: [
+                    'file-loader?name=img/[hash:9]/[hash:9].[ext]',
+                    {
+                        loader: 'image-webpack-loader',
+                        options: {
+                            mozjpeg: {
+                                progressive: true,
+                                quality: 90
+                            },
+                            // optipng.enabled: false will disable optipng
+                            optipng: {
+                                enabled: false,
+                            },
+                            pngquant: {
+                                quality: '75-100',
+                                speed: 5,
+                                strip: true
+                            },
+                            gifsicle: {
+                                interlaced: false,
+                                optimizationLevel: 2
+                            },
+                            svgo: {
+
+                            },
+                            // the webp option will enable WEBP
+                            webp: {
+                                quality: 100,
+                                sharpness: 0,
+                                method: 5,
+                                metadata: 'none',
+                                enabled: true
+                            }
+                        }
+                    }
+                ]
             }
         ]
     },
@@ -48,6 +88,15 @@ module.exports = {
             hash: true,
             template: './src/views/index.html',
             filename: 'index.html'
+        }),
+        new ImageminWebpWebpackPlugin({
+            config: [{
+                test: /\.(jpe?g|png)/,
+                options: {
+                    quality:  90
+                }
+            }],
+            overrideExtension: true
         }),
         new WebpackMd5Hash()
     ]
