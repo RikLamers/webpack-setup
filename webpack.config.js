bdb24cd79caf2304fec87c521f5f039af39d94e1
Rik Lamers <info@riklamers.com>
2019-08-20 14:01:39 +0200

    Added fonts, favicon + cleanup

--- webpack.config.js
+++ webpack.config.js
@@ -4,6 +4,8 @@ const HtmlWebpackPlugin = require('html-webpack-plugin');
 const WebpackMd5Hash = require('webpack-md5-hash');
 const { CleanWebpackPlugin } = require('clean-webpack-plugin');
 const ImageminWebpWebpackPlugin= require("imagemin-webp-webpack-plugin");
+const FaviconsWebpackPlugin = require('favicons-webpack-plugin');
+const RemovePlugin = require('remove-files-webpack-plugin');
 
 module.exports = {
     entry: {
@@ -11,7 +13,7 @@ module.exports = {
     },
     output: {
         path: path.resolve(__dirname, 'build'),
-        filename: process.env === 'production' ? '[name].[chunkhash:9].js' : '[name].[hash:9].js'
+        filename: process.env.npm_lifecycle_event === 'build' ? '[name].[chunkhash:9].js' : '[name].[hash:9].js'
     },
     target: 'web',
     module: {
@@ -69,6 +71,13 @@ module.exports = {
                         }
                     }
                 ]
+            },
+            {
+                test: /\.(woff|woff2|ttf)$/,
+                loader: 'file-loader',
+                options: {
+                    name: 'fonts/[hash:9]/[hash:9][name].[ext]'
+                },
             }
         ]
     },
@@ -79,7 +88,7 @@ module.exports = {
             maxModules: 0
         }
     },
-    devtool: 'source-map',
+    devtool: process.env.npm_lifecycle_event === 'build' ? '' : 'source-map',
     plugins: [
         new CleanWebpackPlugin(),
         new MiniCssExtractPlugin({
@@ -95,11 +104,46 @@ module.exports = {
             config: [{
                 test: /\.(jpe?g|png)/,
                 options: {
+                    exclude: 'favicon/',
                     quality:  90
                 }
             }],
             overrideExtension: true
         }),
-        new WebpackMd5Hash()
+        new FaviconsWebpackPlugin({
+            logo: './src/assets/img/favicon.svg',
+            inject: true,
+            cache: true,
+            outputPath: 'favicon/',
+            prefix: './favicon/',
+            inject: 'force',
+            favicons: {
+                icons: {
+                    android: true,
+                    appleIcon: true,
+                    appleStartup: true,
+                    coast: false,
+                    favicons: true,
+                    firefox: false,
+                    windows: false,
+                    yandex: false
+                }
+            }
+        
+        }),
+        new WebpackMd5Hash(),
+        new RemovePlugin({
+            after: {
+                test: [
+                    {
+                        folder: 'build/favicon',
+                        method: (filePath) => {
+                            return new RegExp(/\.webp$/, 'm').test(filePath);
+                        }
+                    }
+                ]
+            }
+        })
+
     ]
 };
